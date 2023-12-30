5a50a923de7249c5c98ac54fb6a7a799658c7ef0
simge kurtuldu <simgekurtuldu@email.com>
2023-12-30 21:27:14 +0300

    add Employee and HRApplication classes

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,10 @@
+package org.example;
+public class Employee {
+   public void Employees(){
+       Department employee1 = new Department();
+       Department employee2= new Department();
+
+       employee1.getEmployee("Test",1234,15000,100,"Simge Kurtuldu");
+       employee2.getEmployee("Test",1203,10000,25,"Ali Solmaz");
+   }
+}
---
+++ src/main/java/org/example/HRApplication.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class HRApplication {
+    public static void main(String[] args){
+        Employee employee = new Employee();
+        employee.Employees();
+
+    }
+}
